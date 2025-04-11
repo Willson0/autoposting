0f6a4b8b3e18b0b4d747ffa0012a2a7c3cc772dd
Willson0 <logvinovnikita3@gmail.com>
2025-04-11 15:40:20 +0500

    first

--- frontend/src/views/LoginView.vue
+++ frontend/src/views/LoginView.vue
@@ -18,13 +18,13 @@ export default {
             })
                 .then((response) => {
                     if (!response.ok) return alert ("Error");
-                    this.$router.push("/profile");
+                    this.$router.push("/panel");
                 })
         }
         const script = document.createElement('script');
         script.async = true;
         script.src = 'https://telegram.org/js/telegram-widget.js?22';
-        script.setAttribute('data-telegram-login', 'ExoBloomCompanyBot');
+        script.setAttribute('data-telegram-login', 'AutoPostingOfficialBot');
         script.setAttribute('data-size', 'large');
         script.setAttribute('data-onauth', 'onTelegramAuth(user)');
         script.setAttribute('data-request-access', 'write');
